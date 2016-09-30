b546ace47348084baa179b2b05155b5f383aa15a
agodopjr <agodopjr@gmail.com>
2016-09-30 16:29:56 +0800

    Commit files for training

---
+++ helloWorld/src/object/Animal.java
@@ -0,0 +1,39 @@
+package object;
+
+/**
+ * Created by jpmc on 9/28/2016.
+ */
+public class Animal {
+
+    private String animalType;
+    private Integer animalLegs;
+    private Boolean isVertebrate;
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public Integer getAnimalLegs() {
+        return animalLegs;
+    }
+
+    public void setAnimalLegs(Integer animalLegs) {
+        this.animalLegs = animalLegs;
+    }
+
+    public Boolean isVertebrate() {
+        return isVertebrate;
+    }
+
+    public void setAnimalComparison(Boolean animalComparison) {
+        this.isVertebrate = animalComparison;
+    }
+
+
+
+
+}
